9040da733320043563dc0ad5e439c95c1d27c3f5
quantin96 <ngjiaquan3@gmail.com>
2025-02-21 16:50:43 +0800

    Checkstyle changes

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param remark A valid remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
